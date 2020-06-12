f28499385ba8e44411577ef34b618a75547b8416
GerardoFarias <gerardo.farias.galvez@hotmail.com>
2020-06-12 13:43:37 -0400

    Se agregan cors

--- api/comunas.php
+++ api/comunas.php
@@ -1,5 +1,8 @@
 <?php 
     header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     include_once '../service/locationService.php';
     
     
